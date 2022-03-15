80652b4230c7d5fd45734a2cd502637878b833ac
RosaKarin <50404764+RosaKarin@users.noreply.github.com>
2022-03-15 15:04:45 +0100

    ln

--- challenge2.js
+++ challenge2.js
@@ -22,51 +22,13 @@ function tickingTime() {
     s = updateTime(s);
     
     document.getElementById('clock').innerHTML =  h + ":" + m + ":" + s;
-    setTimeout(tickingTime, 1000);
+    setTimeout(tickingTime, 600);
   }
 
   function updateTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    if (i < 10) {i = "0" + i};
     return i;
   }
 
 // Script voor year
 document.getElementById('year').innerHTML = today.getFullYear();
-
-
-// Script voor timezone
-
-function changeGradient(){
-    var now = new Date ();
-    var h =now.getHours();
-
-    if h>= 6 && h > 12{
-        document.getElementById()
-    }
-
-}
-
-// function updateColors(){
-// 	var now = new Date();
-// 	var h = now.getHours();
-		
-// 	if (h >= 6 && h < 12){
-// 		document.getElementById('header-items').style.backgroundImage = 'radial-gradient(circle at top right, #FFEEE0, #FFB97E)';
-// 		document.getElementById('zon').style.backgroundImage = 'linear-gradient(#FFE200, #FF9E6A)';
-// 		document.getElementById('clock').style.boxShadow = '3px 4px 12px -1px #DC9F6D';
-// 	} else if (h >= 12 && h < 18){ 	
-// 		document.getElementById('header-items').style.backgroundImage = 'radial-gradient(circle at top right, #C7EBE3, #6EBEC9)';
-// 		document.getElementById('zon').style.backgroundImage = 'linear-gradient(#FDF980, #EBDF7F)';
-// 		document.getElementById('clock').style.boxShadow = '3px 4px 12px -1px #5FA4AE';
-// 	} else if (h >= 18 && h < 24) {
-// 		document.getElementById('header-items').style.backgroundImage = 'radial-gradient(circle at top right, #F3ECFA, #A4AEEB)';
-// 		document.getElementById('zon').style.backgroundImage = 'linear-gradient(#FD96E8, #F06AA3)';
-// 		document.getElementById('clock').style.boxShadow = '3px 4px 12px -1px #969DCB';
-// 	} else {
-// 		document.getElementById('header-items').style.backgroundImage = 'radial-gradient(circle at top right, #002376, #001340)';
-// 		document.getElementById('zon').style.backgroundImage = 'linear-gradient(#FFFFFF, #CCFFF9)';
-// 		document.getElementById('clock').style.boxShadow = '3px 4px 12px -1px #001037';
-// 	} 
-
-// }
-// 	updateColors();
